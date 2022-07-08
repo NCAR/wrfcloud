4b5756fa6a85d44d61e9923bb5bc7dfcf7ea58e0
Michael Kavulich, Jr <kavulich@ucar.edu>
2022-07-08 10:36:42 -0600

    Re-think the namelist building a little  - Instead of a template namelist, start with a real namelist file    - Read as dictionary with f90nml package, make necessary mods    - print new namelist to working directory
    Modified the example test.yml a bit so we know the modifications are working.

--- python/src/runtime/tools/make_wps_namelist.py
+++ python/src/runtime/tools/make_wps_namelist.py
@@ -7,15 +7,17 @@ Functions for setting up and creating namelist.wps
 import logging
 import f90nml
 
-def make_wps_namelist(runinfo, logger, nml_file='templates/namelist.wps.template'):
+def make_wps_namelist(runinfo, logger, nml_file='domains/test/namelist.wps'):
     """Main routine that sets up and creates namelist.wps"""
     logger.debug(f'Setting up WPS namelist for "{runinfo.name}"')
 
     with open(nml_file) as nml_file_read:
         nml = f90nml.read(nml_file_read)
-    
-    
-
+    #Replace relevant settings
+    print(nml['share']['max_dom'])
+    for domain in range(nml['share']['max_dom']):
+        nml['share']['start_date'][domain] = runinfo.startdate
+        nml['share']['end_date'][domain] = runinfo.enddate
 
     logger.debug(f'Writing WPS namelist file to {runinfo.wd}')
     f90nml.write(nml, runinfo.wd + '/namelist.wps')
