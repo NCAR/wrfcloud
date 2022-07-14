99e247aafcda79b5959d0076d50fe6ff0f041400
David Hahn <hahnd@ucar.edu>
2022-07-14 15:56:56 -0600

    Fixed a couple of pylint and PyCharm complaints: ************* Module wrfcloud.runtime.ungrib wrfcloud/runtime/ungrib.py:63:0: C0303: Trailing whitespace (trailing-whitespace) wrfcloud/runtime/ungrib.py:15:36: W0613: Unused argument 'namelist' (unused-argument)

--- python/src/wrfcloud/runtime/ungrib.py
+++ python/src/wrfcloud/runtime/ungrib.py
@@ -12,7 +12,8 @@ from string import ascii_uppercase
 # Import our custom modules
 from wrfcloud.runtime.tools.make_wps_namelist import make_wps_namelist
 
-def get_grib_input(runinfo, logger, namelist):
+
+def get_grib_input(runinfo, logger):
     """
     Gets GRIB files for processing by ungrib
 
@@ -36,31 +37,32 @@ def get_grib_input(runinfo, logger, namelist):
             data = runinfo.local_data
 
         for entry in data:
-            #Since there may be multiple string entries in runinfo.local_data, we need to parse
-            #each one individually using glob.glob, then append them all together
+            # Since there may be multiple string entries in runinfo.local_data, we need to parse
+            # each one individually using glob.glob, then append them all together
             filelist.extend(sorted(glob.glob(entry)))
         for gribfile in filelist:
             # Gives us GRIBFILE.AAA on first iteration, then GRIBFILE.AAB, GRIBFILE.AAC, etc.
             griblink = 'GRIBFILE.' + "".join(suffixes.__next__())
             logger.debug(f'Linking input GRIB file {gribfile} to {griblink}')
-            os.symlink(gribfile,griblink)
+            os.symlink(gribfile, griblink)
     else:
         logger.debug('Getting GRIB file(s) from S3 bucket')
         logger.warning('Not yet implemented!')
+
 
 def get_files(runinfo, logger, namelist):
     """Gets all input files necessary for running ungrib"""
 
     logger.debug('Getting GRIB input files')
-    get_grib_input(runinfo, logger, namelist)
+    get_grib_input(runinfo, logger)
 
     logger.debug('Getting geo_em file(s)')
     # Get the number of domains from namelist
-    for domain in range(1,namelist['share']['max_dom'] + 1):
-        os.symlink(f'{runinfo.staticdir}/geo_em.d{domain:02d}.nc',f'geo_em.d{domain:02d}.nc')
+    for domain in range(1, namelist['share']['max_dom'] + 1):
+        os.symlink(f'{runinfo.staticdir}/geo_em.d{domain:02d}.nc', f'geo_em.d{domain:02d}.nc')
 
     logger.debug('Getting VTable')
-    
+
 
 def main(runinfo, logger):
     """Main routine that sets up, runs, and monitors ungrib end-to-end"""
@@ -69,7 +71,7 @@ def main(runinfo, logger):
     # Stop execution if experiment working directory already exists
     if os.path.isdir(runinfo.ungribdir):
         errmsg = (f"Ungrib directory \n                 {runinfo.ungribdir}\n                 "
-                   "already exists. Move or remove this directory before continuing.")
+                  "already exists. Move or remove this directory before continuing.")
         logger.critical(errmsg)
         raise FileExistsError(errmsg)
 
@@ -82,6 +84,7 @@ def main(runinfo, logger):
     get_files(runinfo, logger, namelist)
 
     logger.warning(f"{__name__} isn't fully implemented yet!")
+
 
 if __name__ == "__main__":
     print('Script not yet set up for standalone run, exiting...')
