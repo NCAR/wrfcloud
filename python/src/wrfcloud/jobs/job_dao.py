22730e6e883849883439944ea63e3a001661874b
David Hahn <hahnd@ucar.edu>
2022-08-24 13:16:36 -0600

    WRF Job data model and CRUD operations.

---
+++ python/src/wrfcloud/jobs/job_dao.py
@@ -0,0 +1,101 @@
+"""
+The UserDao class is a data access object that performs basic CRUD
+(create, read, update, delete) functions on the user database.
+"""
+
+import os
+import pkgutil
+from typing import Union, List
+import yaml
+from wrfcloud.dynamodb import DynamoDao
+from wrfcloud.jobs import WrfJob
+
+
+class JobDao(DynamoDao):
+    """
+    CRUD operations for jobs
+    """
+
+    def __init__(self, endpoint_url: str = None):
+        """
+        Create the Data Access Object (DAO)
+        :param endpoint_url: (optional) Specifying the endpoint URL can be useful for testing
+        """
+        # load the user table definition
+        self.table_definition = yaml.safe_load(pkgutil.get_data('wrfcloud', 'jobs/table.yaml'))
+
+        # get the table name
+        table_name = os.environ[self.table_definition['table_name_var']]
+
+        # get the key fields for the table
+        key_fields = self.table_definition['key_fields']
+
+        # get the endpoint URL, but do not overwrite the local argument
+        if endpoint_url is None and 'ENDPOINT_URL' in os.environ:
+            endpoint_url = os.environ['ENDPOINT_URL']
+
+        # call the super constructor
+        super().__init__(table_name, key_fields, endpoint_url)
+
+    def add_job(self, job: WrfJob) -> bool:
+        """
+        Store a new user
+        :param job: Job object to store
+        :return: True if successful, otherwise False
+        """
+        # save the item to the database
+        return super().put_item(job.data)
+
+    def get_job_by_id(self, job_id: str) -> Union[WrfJob, None]:
+        """
+        Get a job by job id
+        :param job_id: Job ID
+        :return: The job with the given job id, or None if not found
+        """
+        # build the database key
+        key = {'job_id': job_id}
+
+        # get the item with the key
+        data = super().get_item(key)
+
+        # handle the case where the key is not found
+        if data is None:
+            return None
+
+        # build a new user object
+        return WrfJob(data)
+
+    def get_all_jobs(self) -> List[WrfJob]:
+        """
+        Get a list of all users in the system
+        :return: List of all users
+        """
+        # Convert a list of items into a list of User objects
+        return [WrfJob(item) for item in super().get_all_items()]
+
+    def update_job(self, job: WrfJob) -> bool:
+        """
+        Update the job data
+        :param job: Job data values to update, which must include the key field (job_id)
+        :return: True if successful, otherwise False
+        """
+        return super().update_item(job.data)
+
+    def delete_job(self, job: WrfJob) -> bool:
+        """
+        Delete the job from the database (not any associated data)
+        :param job: WrfJob object
+        :return: True if successful, otherwise False
+        """
+        job_id = job.job_id
+        return super().delete_item({'job_id': job_id})
+
+    def create_job_table(self) -> bool:
+        """
+        Create the job table
+        :return: True if successful, otherwise False
+        """
+        return super().create_table(
+            self.table_definition['attribute_definitions'],
+            self.table_definition['key_schema']
+        )
--- python/test/wrfcloud/helper.py
+++ python/test/wrfcloud/helper.py
@@ -1,7 +1,8 @@
 from typing import List
 from wrfcloud.api.audit import AuditDao
-from wrfcloud.user import UserDao, User
 from wrfcloud.api.auth.refresh import RefreshTokenDao
+from wrfcloud.jobs import JobDao, WrfJob
+from wrfcloud.user import UserDao, User
 import yaml
 
 
@@ -15,19 +16,22 @@ def _test_setup() -> bool:
         user_dao = UserDao()
         audit_dao = AuditDao()
         refresh_dao = RefreshTokenDao()
+        job_dao = JobDao()
 
         try:
             # just in case the table already exists, get rid of it
             user_dao.delete_table(user_dao.table)
             audit_dao.delete_table(audit_dao.table)
             refresh_dao.delete_table(refresh_dao.table)
+            job_dao.delete_table(job_dao.table)
         except Exception:
             pass
 
         # create the table
         return user_dao.create_user_table() and \
             audit_dao.create_audit_table() and \
-            refresh_dao.create_refresh_table()
+            refresh_dao.create_refresh_table() and \
+            job_dao.create_job_table()
     except Exception as e:
         print(e)
         return False
@@ -43,9 +47,11 @@ def _test_teardown() -> bool:
         user_dao = UserDao()
         audit_dao = AuditDao()
         refresh_dao = RefreshTokenDao()
+        job_dao = JobDao()
         user_dao.delete_table(user_dao.table)
         audit_dao.delete_table(audit_dao.table)
         refresh_dao.delete_table(refresh_dao.table)
+        job_dao.delete_table(job_dao.table)
     except Exception as e:
         print(e)
         return False
@@ -90,3 +96,40 @@ def _get_all_sample_users() -> List[User]:
 
     # return the sample users
     return users
+
+
+def _get_sample_job(status_code: int) -> WrfJob:
+    """
+    Get a sample job with a given status
+    :return: A sample job object
+    """
+    # load sample users
+    sample_jobs = yaml.load(open('resources/sample_jobs.yaml'))
+
+    # get the first admin entry
+    job_data = sample_jobs['jobs'][status_code][0]
+    return WrfJob(job_data)
+
+
+def _get_all_sample_jobs(status_code: int = None) -> List[WrfJob]:
+    """
+    Get a sample job with a given status
+    :param status_code: See wrfcloud.jobs.WrfJob.STATUS_CODE_* for definitions
+    :return: A sample job object
+    """
+    # load sample jobs
+    sample_jobs = yaml.load(open('resources/sample_jobs.yaml'))['jobs']
+    jobs_data = []
+
+    # get jobs of all status codes if status code is not provided...
+    if status_code is None:
+        for sc in range(0, 6):
+            for job_data in sample_jobs[sc]:
+                jobs_data.append(job_data)
+    # ...otherwise, only get the job data for the requested status code
+    else:
+        for job_data in sample_jobs[status_code]:
+            jobs_data.append(job_data)
+
+    # build job objects and return them
+    return [WrfJob(job_data) for job_data in jobs_data]
