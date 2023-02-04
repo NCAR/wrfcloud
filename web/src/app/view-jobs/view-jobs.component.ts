28a398543f8822d566c07f860cad3b979ba64c41
David Hahn <hahnd+ghpgp@ucar.edu>
2023-02-03 18:16:55 -0700

    Updated distribution IDs for most recent re-deployment.

--- web/src/app/view-jobs/view-jobs.component.ts
+++ web/src/app/view-jobs/view-jobs.component.ts
@@ -147,9 +147,6 @@ export class ViewJobsComponent implements OnInit, AfterViewInit, OnDestroy, Webs
     /* handle the response */
     if (response.ok)
     {
-      // TODO: Get rid of this nonsense
-      for (let job of response.data.jobs)
-        job.configuration_name = 'Caribbean 6km';
       this.jobs = response.data.jobs;
       this.dataSource.data = this.jobs;
     }
