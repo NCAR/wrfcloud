b50edeccab9c0cedaef3e9a989f7a7c455b56c0a
hahnd <7718310+hahnd@users.noreply.github.com>
2023-03-27 09:57:20 -0600

    46-height-selector (#175)
    * Added labels to the pressure level selector.
    
    * Set default pressure level to the lowest level.
    
    ---------
    
    Co-authored-by: David Hahn <>

--- web/src/app/wrf-viewer/wrf-viewer.component.ts
+++ web/src/app/wrf-viewer/wrf-viewer.component.ts
@@ -117,14 +117,14 @@ export class WrfViewerComponent implements OnInit
   /**
    * List of valid height value selections
    */
-  public validHeights: number[] = [1000, 925, 850, 700, 500, 300, 250, 100];
+  public validHeights: number[] = [100, 250, 300, 500, 700, 850, 925, 1000];
 
 
   /**
    * Initialize the layer request
    */
   public req: LayerRequest = {
-    height: this.validHeights[0]
+    height: this.validHeights[this.validHeights.length - 1]
   };
 
 
@@ -689,7 +689,7 @@ export class WrfViewerComponent implements OnInit
   }
 
   /**
-   *
+   * Handle an event from the height selector changing
    * @param event
    */
   public heightChanged(event: MatSliderChange): void
